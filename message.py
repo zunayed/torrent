870632a5e9893ebb45aea0532f195253788777c1
Shakiur <shakiur@gmail.com>
2014-08-30 01:05:21 -0400

    Initial commit of message class

---
+++ message.py
@@ -0,0 +1,8 @@
+class Message(object):
+	def __init__(self, length, msg, msg_type):
+		self.length = length
+		self.msg = msg
+		self.msg_type = msg_type
+
+	def checkLength(self):
+		return self.length == len(self.msg)
