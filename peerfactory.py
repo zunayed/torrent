d0161571013a3bee7ac01bcf9fed46235fa113e1
Shakiur <shakiur@gmail.com>
2014-08-30 00:15:29 -0400

    Add function to store full & partial messages in a list, and read them only when they are complete

--- peerfactory.py
+++ peerfactory.py
@@ -1,13 +1,17 @@
 from twisted.internet import protocol
+from message import Message
 
 
-class PeerProtocol(protocol.Protocol):
+class Peer(protocol.Protocol):
 
     def __init__(self, factory, handshake, torrent):
         self.factory = factory
         self.handshake = handshake
         self.torrent = torrent
+
+        self.message_list = []
         self.interested_msg = 3 * chr(0) + chr(1) + chr(2)
+        
         self.peer_state = "HANDSHAKE"
         self.total_pieces = 0
         self.peer_pieces = []
@@ -15,69 +19,159 @@ class PeerProtocol(protocol.Protocol):
     def connectionMade(self):
         self.factory.numConnections += 1
 
-        print "Send Handshake: %r" % self.handshake
+        # print "Send Handshake: %r" % self.handshake
         self.transport.write(self.handshake)
 
     def dataReceived(self, data):
-       # Check to find type of message
+        # Take data and push it to message list
+        print "Data Received: %r" % data
+        self.populateMessageList(data)
+        
+        while(self.message_list and self.message_list[0].checkLength()):  
+            temp = self.returnNextMessage()
+            print "Type: %r" % temp.msg_type
+            print "Full Message: %r" % temp.msg
 
-        if data[4] == chr(0):
-            print "Choke Message: %r" % data
+            if temp.msg_type == "BITFIELD":
+                self.total_pieces = ord(data[3])-1
+                print "Peer total pieces: %r" % self.total_pieces
 
-        elif data[4] == chr(1):
-            print "Unchoke Message: %r" % data
-            self.peer_state = "UNCHOKE"
+                # Converts each byte to a string of bits (temp_binary_piece)
+                for i in range(5,5+self.total_pieces):
+                    temp_binary_piece = bin(ord(data[i]))[2:].rjust(8,'0')
 
-            # send request message here
+                    # Stores each bit so we know which piece peer has
+                    for j in range(0,8):
+                        self.peer_pieces.append(temp_binary_piece[j])
+
+                # Send Unchoke Message
+                print "Send unchoke msg after bitfield msg"
+                self.transport.write(self.interested_msg)
+
+            elif temp.msg_type == "HAVE":
+                pass
+
+            elif temp.msg_type == "KEE_ALIVE":
+                pass
+
+            elif temp.msg_type == "CHOKE":
+                pass
+
+            elif temp.msg_type == "UNCHOKE":
+                pass
+
+            elif temp.msg_type == "INTERESTED":
+                pass
+
+            elif temp.msg_type == "NOT_INTERESTED":
+                pass
 
             import ipdb
             ipdb.set_trace()
 
-        elif data[4] == chr(2):
-            print "Interested Message: %r" % data
 
-        elif data[4] == chr(3):
-            print "Non-Interested Message: %r" % data
+    def populateMessageList(self,data):
+        # Function to push either partial or full msg to list
 
-        elif data[4] == chr(4):
-            print "Have Message: %r" % data
+        if(self.peer_state == "HANDSHAKE"):
+            self.addHandshake(data)
 
-        elif data[4] == chr(5):
-            print "Bitfield Message: %r" % data
-            
-            self.total_pieces = ord(data[3])-1
-            print "Peer total pieces: %r" % self.total_pieces
+        elif(self.peer_state == "MESSAGE"):
 
-            # Converts each byte to a string of bits (temp_binary_piece)
-            for i in range(5,5+self.total_pieces):
-                temp_binary_piece = bin(ord(data[i]))[2:].rjust(8,'0')
+            if(self.message_list):
 
-                # Stores each bit so we know which piece peer has
-                for j in range(0,8):
-                    self.peer_pieces.append(temp_binary_piece[j])
+                if(self.message_list[-1].checkLength()):
+                    # Last message is complete, add new message
+                    self.addNewMessage(data)
+                else:
+                    # Last message is not complete, insert rest of message
+                    self.addIncompleteMessage(data)
+            else:
+                # No messages, add new message
+                self.addNewMessage(data)
 
-            # Send Unchoke Message
-            print "Send unchoke msg after bitfield msg"
-            self.transport.write(self.interested_msg)
+    def addIncompleteMessage(self,data):
+        msg_full_len = self.message_list[-1].length
+        msg_1_len = len(self.message_list[-1].msg)
+        msg_2_len = msg_full_len - msg_1_len
+
+        total_size = len(data)
+
+        if(total_size == msg_2_len):
+            self.message_list[-1].msg += data
+        elif(total_size > msg_2_len):
+            self.message_list[-1].msg += data[0:msg_2_len]
+            self.populateMessageList(data[msg_2_len:total_size])
+        else:
+            self.message_list[-1].msg += data
+
+    def addNewMessage(self, data):
+        if(data[3] == chr(0)):
+            msg_type = "KEEP_ALIVE"
+            msg_size = 4
+
+        elif(data[4] == chr(0)):
+            msg_type = "CHOKE"
+            msg_size = 5
+
+        elif(data[4] == chr(1)):
+            msg_type = "UNCHOKE"
+            msg_size = 5
+
+        elif(data[4] == chr(2)):
+            msg_type = "INTERESTED"
+            msg_size = 5
+
+        elif(data[4] == chr(3)):
+            msg_type = "NOT_INTERESTED"
+            msg_size = 5
+
+        elif(data[4] == chr(4)):
+            msg_type = "HAVE"
+            msg_size = 5
+
+        elif(data[4] == chr(5)):
+            msg_type = "BITFIELD"
+            msg_size = (ord(data[3])-1)+5
+
+        total_size = len(data)
+
+        if(total_size == msg_size):
+            temp = Message(msg_size, data, msg_type)
+            self.message_list.append(temp)
+        elif(total_size > msg_size):
+            temp = Message(msg_size, data[0:msg_size],msg_type)
+            self.message_list.append(temp)
+            self.populateMessageList(data[msg_size:total_size])
+        else:
+            temp = Message(msg_size, data[0:total_size], msg_type)
+            self.message_list.append(temp)
+
+    def addHandshake(self,data):
+        total_size = len(data)
+        hndshk_size = ord(data[0])+49
+
+        if(len(data) == hndshk_size):
+            temp = Message(hndshk_size,data,"HANDSHAKE")
+            self.message_list.append(temp)
+            self.peer_state = "MESSAGE"
+
+        elif(len(data) > hndshk_size):
+            temp = Message(hndshk_size,data[0:hndshk_size],"HANDSHAKE")
+            self.message_list.append(temp)
+            self.peer_state = "MESSAGE"
+            self.populateMessageList(data[hndshk_size:total_size])
 
         else:
-            print "Handshake Message: %r" % data
-            if self.checkHandshake(data):
-                self.peer_state = "CHOKE"
-                print "Send Interested Message: %r" % self.interested_msg
-                self.transport.write(self.interested_msg)
+            temp = Message(hndshk_size,data[0:total_size],"HANDSHAKE")
+            self.messsage_list.append(temp)
         
-    def checkHandshake(self, data):
-        # handshake: <pstrlen><pstr><reserved><info_hash><peer_id>
-        pstrlen = ord(data[0])
-        pstr = data[1:pstrlen + 1]
-        reserved = data[pstrlen + 1:pstrlen + 1 + 8]
-        info_hash = data[pstrlen + 1 + 8:pstrlen + 1 + 8 + 20]
-        peer_id = data[pstrlen + 1 + 8 + 20:pstrlen + 1 + 8 + 20 + 20]
 
-        # Put code here to check if we want to connect to peer
-
-        return True
+    def returnNextMessage(self):
+        if(self.message_list[0].checkLength()):
+            return self.message_list.pop(0)
+        else:
+            return 0
 
 
 class PeerFactory(protocol.ClientFactory):
@@ -88,7 +182,7 @@ class PeerFactory(protocol.ClientFactory):
         self.torrent = torrent
 
     def buildProtocol(self, addr):
-        return PeerProtocol(self, self.handshake, self.torrent)
+        return Peer(self, self.handshake, self.torrent)
 
     def clientConnectionFailed(self, connector, reason):
         print 'connection failed:', reason.getErrorMessage()
