6ab286bf97e40a9adfee9116ef535515d6721c34
Zunayed Ali <zunayed@gmail.com>
2014-09-02 20:15:14 -0400

    format

--- message.py
+++ message.py
@@ -1,8 +1,9 @@
 class Message(object):
-	def __init__(self, length, msg, msg_type):
-		self.length = length
-		self.msg = msg
-		self.msg_type = msg_type
 
-	def checkLength(self):
-		return self.length == len(self.msg)
+    def __init__(self, length, msg, msg_type):
+        self.length = length
+        self.msg = msg
+        self.msg_type = msg_type
+
+    def checkLength(self):
+        return self.length == len(self.msg)
